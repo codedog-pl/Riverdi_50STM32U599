543f5ab0bc057a412542b7922c5b110b739196e1
Adam Łyskawa <a.lyskawa@gmail.com>
2023-12-15 00:01:56 +0100

    FIX: Ensure that the deleted constructors and destructors are public.

--- Tools/c/StaticClass.hpp
+++ Tools/c/StaticClass.hpp
@@ -2,6 +2,7 @@
 
 // Makes the class static by deleting implicit constructors and destructor.
 #define STATIC(className)\
+public:\
     className() = delete;\
     className(const className&) = delete;\
     className(className&&) = delete;\
